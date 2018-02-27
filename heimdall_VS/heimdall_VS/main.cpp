2ac4d205e5b1d9f0a251b321f38944b9e56ddeed
Martin Antonsson <maran731@student.liu.se>
2018-02-27 14:30:37 +0100

    OpenCV installerat

--- heimdall_VS/heimdall_VS/main.cpp
+++ heimdall_VS/heimdall_VS/main.cpp
@@ -1,10 +1,14 @@
 #include "heimdall_VS.h"
 #include <QtWidgets/QApplication>
 
+using namespace std;
+using namespace cv;
+
 int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	heimdall_VS w;
 	w.show();
+
 	return a.exec();
 }
