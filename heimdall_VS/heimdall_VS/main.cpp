89da764ecddd9c4aa9a1c87fc7c145268f3a22e6
Nisse <tbmt41projekt@gmail.com>
2018-04-11 09:45:18 +0200

    hej

--- heimdall_VS/heimdall_VS/main.cpp
+++ heimdall_VS/heimdall_VS/main.cpp
@@ -1,5 +1,5 @@
-#include "Engine.h"
-using namespace std;
+//#include "Engine.h"
+//using namespace std;
 
 //__________run()_________________________________________________________________________________
 
@@ -8,11 +8,14 @@ using namespace std;
 //Körs igång när programmet startas, dens uppgift är att skapa en ny Engine-klass och kalla på
 //dess run-funktion. Den väntar sedan på att en int ska returneras, detta görs från run-funktionen.
 
-int main(int argc, char *argv[])
-{
-	Engine engine{ argc, argv };
-	engine.run();
-}
+//int main(int argc, char *argv[])
+//{
+//	/*Engine engine{ argc, argv };
+//	engine.run();*/
+//
+//
+//	return 0;
+//}
 
 //________________________________________________________________________________________________
 
@@ -157,3 +160,47 @@ int main(int argc, char *argv[])
 //
 //	return 0;
 //}
+
+#include "matlabLibrary.h"
+
+int main(int argc, char** argv) {
+
+	mclmcrInitialize();
+	if (!mclInitializeApplication(NULL, 0)) {
+
+		std::cerr << "could not initialize the application properly"
+			<< std::endl;
+		return -1;
+	}
+	if (!matlabLibraryInitialize()) {
+		std::cerr << "could not initialize the library properly"
+			<< std::endl;
+		return -1;
+	}
+	else {
+		try {
+			double data = 2;
+			mwArray in(1, 1, mxDOUBLE_CLASS, mxREAL);
+			in.SetData(&data, 1);
+
+			mwArray out;
+			square(1, out, in);
+			std::cout << "The value is " << out << std::endl;
+
+		}
+		catch (const mwException& e) {
+			std::cerr << e.what() << std::endl;
+			return -2;
+		}
+		catch (...) {
+			std::cerr << "Unexpected error thrown" << std::endl;
+			return -3;
+		}
+		matlabLibraryTerminate();
+	}
+	mclTerminateApplication();
+	return 0;
+
+
+
+}
